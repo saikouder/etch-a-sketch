1b4df4213791c6520e57384ef1455cb3c785223d
Anthony <139452270+saikouder@users.noreply.github.com>
2024-03-13 03:40:17 -0700

    Implement initial function for coloring squares

--- scripts.js
+++ scripts.js
@@ -6,10 +6,16 @@ function createGrid() {
         for (let i=0; i<15; i++) {
             let newTile = document.createElement("div");
             newTile.setAttribute("class", "tile");
+            newTile.onmouseover = fillSquare;
             newTileRow.appendChild(newTile);
         }
         document.getElementById("gridContainer").appendChild(newTileRow);
     }
 }
+
+function fillSquare() {
+    this.style.backgroundColor = "black";
+}
+
 
 createGrid();
