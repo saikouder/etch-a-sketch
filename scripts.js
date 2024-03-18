65f378621c9ccc174c700c390cb3efc24776f5e0
Anthony <139452270+saikouder@users.noreply.github.com>
2024-03-17 19:56:41 -0700

    Fix issue of grid overflowing its container

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 function createGrid(tilesPerRow = 16) {
     for (let i=1; i<tilesPerRow; i++) {
+        let newTileWrapper = document.createElement("div");
+        newTileWrapper.setAttribute("class", "row-wrapper");
         let newTileRow = document.createElement("div");
         newTileRow.setAttribute("class", "grid-row");
         console.log(newTileRow);
@@ -9,14 +11,15 @@ function createGrid(tilesPerRow = 16) {
             newTile.onmouseover = fillSquare;
             newTileRow.appendChild(newTile);
         }
-        document.getElementById("gridContainer").appendChild(newTileRow);
+        newTileWrapper.appendChild(newTileRow);
+        document.getElementById("gridContainer").appendChild(newTileWrapper);
     }
 }
 
 function resetGrid() {
     let gridSideLength = Number(prompt("How many tiles per row & column? (max is 60)", "20"));
-    if (gridSideLength > 60) {
-        gridSideLength = 60;
+    if (gridSideLength > 64) {
+        gridSideLength = 64;
     }
     let usedGrid = document.getElementById("gridContainer");
     usedGrid.innerHTML = "";
